e454b38acef2edc8fa191532316661fcfa2ddec3
luciano kruk <sp04@kruk.eng.br>
2025-09-29 23:54:00 +0200

    start of the actual solution (object)

--- kltisystems/kNOrderDerivativeSiso.py
+++ kltisystems/kNOrderDerivativeSiso.py
@@ -14,30 +14,52 @@ GitHub:
 """
 #>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>
 ##WWww=--  import section: --=wwWW##
-import numpy           as np
-#from   numpy           import dot
-#from   numpy           import inf
-#from   scipy.integrate import odeint
+import numpy            as np
 import scipy.signal     as ss
-from scipy.signal       import ss2tf
-import matplotlib.pylab as plt
+
+#>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>#
+#                                                                                  #
+#>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>#
+class kNOrderDerivativeSiso:
+    """
+    This objects smoothes the input signal and calculates its derivatives.
+
+    \parameters:                     
+        order    : maximum order of the derivative to estimate; eg. y''' => order=3
+        pole     : poles of the system, negative floats
+        Ts       : discrete interval
+    """
+
+    def __init__(self, order, pole, Ts):
+        assert order > 0
+        assert pole < 0
+        assert Ts > 0
+
+        self.M    = order + 1
+        self.pole = pole
+        self.Ts   = Ts
 
 #################################
 ## ##WWww=--  main:  --=wwWW## ##
 #################################
 if __name__ == "__main__":
+
+    import matplotlib.pylab as plt
+
     pole = -100
     order = 4
-    Fs = 100 # [Hz]
+    Fs = 400 # [Hz]
     Ts = 1./Fs
     U = list()
 
+    # time and input signal:
     for i in range(4):
         v = np.random.randn()
         U.append( [v for i in range(Fs)] )
     U = np.hstack(U)
     T = np.asarray(range(len(U))) * Ts
 
+    # polynomial:
     den = np.poly1d( [1, -pole] )
     for i in range(order-1):
         den *= np.poly1d( [1, -pole] )
@@ -61,6 +83,18 @@ if __name__ == "__main__":
         C[i] = 1.0
         sys = ss.lti(A,B,C,D)
         _, sysOut, _ = ss.lsim(sys, U, T)
+        ax[i].plot(T, sysOut)
+
+        # discrete:
+        sysd = ss.cont2discrete((A,B,C,D), Ts)
+        b, a = ss.ss2tf(sysd[0], sysd[1], sysd[2], sysd[3])
+        b = b.squeeze() # bug?
+        sysOut = list()
+        out, state = ss.lfilter(b, a, [0], zi=ss.lfiltic(b,a,[]))
+        for j in U:
+            out, state = ss.lfilter(b,a, [j], zi=state)
+            sysOut.append(float(out))
+
         ax[i].plot(T, sysOut)
         ax[i].grid(True)
 
